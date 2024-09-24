52056ac95589483a7db5e81090e9d9b72f807007
jjabar <proihee@gmail.com>
2024-09-24 18:02:50 +0100

    text error

--- python-iot-gateway/data_generator.py
+++ python-iot-gateway/data_generator.py
@@ -16,7 +16,7 @@ def generate_message() -> dict:
     return [ 
         {
             "sensorTag": "sensor0001",
-            'slug': message,
+            'slug': messages, 
             "longitude":  random.choice(points)[1],
             "latitude":  random.choice(points)[0],
             "temperature": random.choice(temperatures),
